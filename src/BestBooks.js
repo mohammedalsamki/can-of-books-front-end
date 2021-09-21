1a608dde6802cf2e447d6ada362912b43bd708ed
mohammedalsamki <momosamak21@gmail.com>
2021-09-21 13:26:30 -0400

    compleat the frontend

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import Carousel from "react-bootstrap";
 import BookFormModel from "./BookFormModal";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
@@ -17,134 +16,147 @@ export class BestBooks extends Component {
         this.state = {
             bookData: [],
             displayModel: false,
-
+            title: '',
+            description: '',
+            status: '',
+            email: '',
+            id: ''
         };
     }
 
     displayModel = () => {
         this.setState({ displayModel: !this.state.displayModel })
     }
-    componentDidMount = async () => {
-        axios.get(`${process.env.REACT_APP_API_URL}/books`)
-            .then((bookResponse) => {
-                this.setState({ bookData: bookResponse.data });
+ 
+    componentDidMount = () => {
+        axios.get(`${process.env.REACT_APP_API_URL}/books/data`)
+            .then(res => {
+                this.setState({ bookData:res.data });
             })
 
-
-            .catch((error) => alert(error.message));
-        console.log(this.state.bookData);
-    };
-    // submithandler = (e) => {
-    //     e.preventDefault();
-    //     const reqBody = {
-    //         title: e.target.booknames.value,
-    //         description: e.target.booksDescription.value,
-    //         status: e.target.booksStatus.value,
-    //         email: e.target.email.value,
-    //     }
-
-
-    //     axios
-    //         .post(`${process.env.REACT_APP_API_URL} /books`, reqBody).then((resBooks) => {
-
-    //             this.state.bookData.push(resBooks.data)
-    //             this.setState({ bookData: this.state.bookData });
-    //             this.displayModel();
-    //         })
-    //         .catch(() => alert("something Wrong"));
-    // }
-    submithandler=(e)=>{
-        e.preventDefault();
-        let config={
-          method:"POST",
-          baseURL:`${process.env.REACT_APP_API_URL}`,
-          url:"/books",
-          data:{
-            title: e.target.booknames.value,
-             description: e.target.booksDescription.value,
-             status: e.target.booksStatus.value,
-             email: e.target.email.value,
-          }
-          
         }
-        axios(config).then(response=>{
-          this.setState({
-            catsList:response.data
-          })
-        })
-      }
-    // deleteHandler= (bookId)=>{
-    //     axios.delete(`${process.env.REACT_APP_API_URL}/books/${bookId}`)
-    //     .then((deleteres)={
-    //         if (deleteres.data.deletedCount ===1){
+        handleTitle = (e) => { this.setState({ title: e.target.booknames.value }) };
+        descriptionHandler = (e) => { this.setState({ description: e.target.booksDescription.value }) };
+        statusHandler = (e) => { this.setState({ status: e.target.booksStatus.value }) };
+        emailHandler = (e) => { this.setState({ email: e.target.email.value }) }
 
-    //             const booksArrNew = this.state.bookData.filter(
-    //                 (book)=>book._id !==bookId
-    //             );
-    //             this.setState({bookData:booksArrNew});
-    //         }
-    //     }).catch(()=>alert("erroe mass"));
-    // }
-    handleDelete = (id) => {
-        let bookId = id;
-        let config = {
-            method: "DELETE",
-            baseURL: `${process.env.REACT_APP_API_URL}`,
-            url: `/books/${bookId}`,
 
-        }
 
-        axios(config).then(response => {
-            this.setState({
-                catsList: response.data
+        submithandler = (e) => {
+            e.preventDefault();
+            let config = {
+                method: "POST",
+                baseURL: process.env.REACT_APP_BACKEND_URL,
+                url: `/books`,
+                data: {
+                    title: this.state.title,
+                    description: this.state.description,
+                    status: this.state.status,
+
+                    email: this.state.email
+                }
+            }
+            axios(config).then(res => {
+                console.log(res.data)
+                this.setState({
+                    bookData: res.data
+                })
             })
-        })
+            console.log(this.state.bookData);
+        }
+        // submithandler=(e)=>{
+        //     e.preventDefault();
+        //     let config={
+        //       method:"POST",
+        //       baseURL:`${process.env.REACT_APP_API_URL}`,
+        //       url:"/books",
+        //       data:{
+        //         title: e.target.booknames.value,
+        //          description: e.target.booksDescription.value,
+        //          status: e.target.booksStatus.value,
+        //          email: e.target.email.value,
+        //       }
+
+        //     }
+        //     axios(config).then(response=>{
+        //       this.setState({
+        //         catsList:response.data
+        //       })
+        //     })
+        //   }
+        // deleteHandler= (bookId)=>{
+        //     axios.delete(`${process.env.REACT_APP_API_URL}/books/${bookId}`)
+        //     .then((deleteres)={
+        //         if (deleteres.data.deletedCount ===1){
+
+        //             const booksArrNew = this.state.bookData.filter(
+        //                 (book)=>book._id !==bookId
+        //             );
+        //             this.setState({bookData:booksArrNew});
+        //         }
+        //     }).catch(()=>alert("erroe mass"));
+        // }
+        handleDelete = (id) => {
+            let bookId = id;
+            let config = {
+                method: "DELETE",
+                baseURL: `${process.env.REACT_APP_API_URL}`,
+                url: `/books/${id}`,
+
+            }
+
+            axios(config).then(response => {
+                this.setState({
+                    catsList: response.data
+                })
+            })
+        }
+
+        render() {
+            return (
+                <div>
+                    <Button onClick={this.displayModel}>add new book</Button>
+
+                    {this.state.displayModel &&
+
+                        <BookFormModel
+                            show={this.state.displayModel}
+                            handleClos={this.displayModel}
+                            submithandler={this.submithandler}
+                        />
+                    }
+
+                    {this.state.bookData.length > 0 &&
+                        <>
+                            {this.state.bookData.map((items,index) => {
+                                return (
+                                    
+                                        <Card key={index}>
+                                            <Card.Body>
+                                                
+                                                <Card.Title>{items.title}</Card.Title>
+                                                <Card.Text>{items.description}</Card.Text>
+                                                <Card.Text>{items.status}</Card.Text>
+                                                <Card.Text>{items.email}</Card.Text>
+                                            </Card.Body>
+                                            <Button variant="danger"
+                                                onClick={() => this.handleDelete(items._id)}
+                                            > Delete item</Button>
+                                        </Card>
+                                    
+                                )
+                            })}
+
+                        </>
+                    }
+
+                </div>
+
+
+            )
+
+        }
     }
-
-    render() {
-        return (
-            <div>
-                <Button onClick={this.displayModel}>add new book</Button>
-
-                {this.state.displayModel &&
-
-                    <BookFormModel
-                        show={this.state.displayModel}
-                        handleClos={this.displayModel}
-                        submithandler={this.displayModel}
-                    />
-                }
-
-                {this.state.bookData.length > 0 &&
-                    <>
-                        {this.state.bookData.map((items) => {
-                            return (
-                                <>
-                                    <Card >
-                                        <Card.Body>
-                                            <Card.Title>{items.title}</Card.Title>
-                                            <Card.Text>{items.description}</Card.Text>
-                                            <Card.Text>{items.status}</Card.Text>
-                                            <Card.Text>{items.email}</Card.Text>
-                                        </Card.Body>
-                                        <Button variant="danger"
-                                            onClick={() => this.handleDelete(items._id)}
-                                        > Delete item</Button>
-                                    </Card>
-                                </>
-                            )
-                        })}
-
-                    </>
-                }
-
-            </div>
-
-
-        )
-
-    }
-}
 
 
 export default BestBooks
--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -4,7 +4,18 @@ import Button from 'react-bootstrap/Button'
 
 import React, { Component } from 'react'
 
+
+
+
 export class BookFormModal extends Component {
+
+
+
+    
+    handleTitle = (e) => { this.setState({ title: e.target.booknames.value }) };
+        descriptionHandler = (e) => { this.setState({ description: e.target.booksDescription.value }) };
+        statusHandler = (e) => { this.setState({ status: e.target.booksStatus.value }) };
+        emailHandler = (e) => { this.setState({ email: e.target.email.value }) }
     render() {
         return (
             <div>
@@ -19,30 +30,30 @@ export class BookFormModal extends Component {
 
                             <Form.Group controlId="formEmail">
                                 <Form.Label>Book Name</Form.Label>
-                                <Form.Control tybe="text" name="booknames" />
+                                <Form.Control tybe="text" name="booknames" onChange={this.props.title}/>
                             </Form.Group>
 
                             <Form.Group controlId="formEmail">
                                 <Form.Label>Book Description</Form.Label>
-                                <Form.Control type="text" name="booksDescription" />
+                                <Form.Control type="text" name="booksDescription" onChange={this.props.description} />
                             </Form.Group>
                             <Form.Group controlId="formEmail">
                                 <Form.Label>Book Status</Form.Label>
-                                <Form.Control type="text" name="booksStatus" />
+                                <Form.Control type="text" name="booksStatus" onChange={this.props.status}/>
                             </Form.Group>
                             <Form.Group controlId="formEmail">
                                 <Form.Label>Your Email</Form.Label>
-                                <Form.Control type="text" name="email" />
+                                <Form.Control type="text" name="email" onChange={this.props.email}/>
                             </Form.Group>
-                            <Button variant="primary" type="submit">
+                            <Button variant="primary" type="submit" onClick={this.props.submithandler}>
                                 Submit
                             </Button>
 
                         </Form>{" "}
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary">Close it</Button>
-                        <Button variant="primary">Save Changes</Button>
+                        <Button variant="secondary" onClick={this.props.handleClos}>Close it</Button>
+                        <Button variant="primary" onClick={this.props.submithandler}>Save Changes</Button>
                     </Modal.Footer>
 
 
